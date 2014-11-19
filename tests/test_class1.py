6e614afc4e31254473403b7d8cd601c3eda4a7ba
ferdinandvwyk <ferdinandvwyk@gmail.com>
2014-11-19 12:15:15 +0000

    ---cleaned init. Found way of using pytest fixtures.

--- tests/test_class1.py
+++ tests/test_class1.py
@@ -3,17 +3,18 @@ import pytest
 
 class TestClass:
 
-    def test_cond_1(self):
-        prius = Car("Prius", "2004", "white")
+    @pytest.fixture(scope='function')
+    def prius(self):                                                      
+        return Car("Prius", "2004", "white")
+
+    def test_cond_1(self, prius):
         assert prius.condition('test') == "New"
 
-    def test_cond_2(self):
-        prius = Car("Prius", "2004", "white")
+    def test_cond_2(self, prius):
         prius.drive(1000)
         assert prius.condition('test') == "Used"
 
-    def test_drive(self):
-        prius = Car("Prius", "2004", "white")
+    def test_drive(self, prius):
         prius.drive(1000)
         assert prius.mileage == 1000
 
